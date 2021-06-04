66801c6665941ee327841dc609f94df6d61ddf72
f73chen <grace.fenglin@gmail.com>
2021-06-03 23:50:26 -0400

    Update humanoid_single.py

--- humanoid_single.py
+++ humanoid_single.py
@@ -13,7 +13,7 @@ from stable_baselines3.common.policies import ActorCriticPolicy
 from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 
-v = "4"
+v = "6"
 d = "humanoid"
 log_path = f"{d}/humanoid_log_{v}/"
 
@@ -21,16 +21,21 @@ env = gym.make('HumanoidBulletEnv-v0')
 # env.render()
 
 # '''
-policy_kwargs = dict(activation_fn=th.nn.ReLU,
-                     net_arch=[dict(pi=[256,256,256,256], vf=[256,256,256,256])])
-model = PPO('MlpPolicy', env, verbose=0, policy_kwargs=policy_kwargs, tensorboard_log=log_path)
-model.learn(total_timesteps=30000000)
+def learning_rate(frac):
+    return 1.0e-4*(np.exp(6*(frac-1)))
+
+policy_kwargs = dict(activation_fn=th.nn.Tanh,
+                     net_arch=[dict(pi=[256,256,192,128], vf=[256,256,192,128])])
+model = PPO('MlpPolicy', env, verbose=0, policy_kwargs=policy_kwargs, learning_rate=learning_rate, tensorboard_log=log_path)
+# model.learn(total_timesteps=30000000)
+model.learn(total_timesteps=300)
 
 # Test saving and loading
 model.save(f"{d}/humanoid_l{v}")
 del model
 # '''
 
+'''
 model = PPO.load(f"{d}/humanoid_l{v}", env = env)
 obs = env.reset()
 for i in range(1000):
@@ -40,3 +45,4 @@ for i in range(1000):
     time.sleep(1/200)
     if done:
         obs = env.reset()
+'''
