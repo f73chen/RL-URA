b98856ae3f950658a1a548c47c829330bf28c865
f73chen <grace.fenglin@gmail.com>
2021-07-10 13:16:33 -0400

    Able to train

--- OpenSim/Modified Osim-rl for Grace/state_init_modified.py
+++ OpenSim/Modified Osim-rl for Grace/state_init_modified.py
@@ -15,6 +15,19 @@ from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 
 from osim.env.osimMod36d import L2RunEnvMod
 
+params = {'reward_weight': [6.0, 1.0, 1.0, 0.4, 0.0, 1.0, 1.0, 0.0, 0.5],
+          #['forward', 'survival', 'torso', 'joint', 'stability', 'act', 'footstep', 'jerk', 'slide']
+          'action_limit': [1]*18,
+          'time_limit': 1000,
+          'stepsize': 0.01,
+          'integrator_accuracy': 5e-3,
+          'seed': 0,
+          'num_cpu': 12,
+          'lr_a1': 1.0e-4,
+          'lr_a2': 2, 
+          'target_speed_range': [0.8,1.2],
+          'total_timesteps': 100000}
+
 v = "v1"
 d = "muscle"
 log_path = f"{d}/muscle_log_{v}/"
@@ -22,35 +35,43 @@ log_path = f"{d}/muscle_log_{v}/"
 def learning_rate(frac):
     return 1.0e-4*(np.exp(6*(frac-1)))
 
+def own_policy(obs):
+    action = np.zeros(18)
+    return action
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 traj_path = dir_path + "\\" + "tracking_solution_fullStride.sto"
 
-env = L2RunEnvMod(visualize=True, traj_path=traj_path)
-# env = L2RunEnvMod(visualize=False, traj_path=traj_path)
+env = L2RunEnvMod(reward_weight=params['reward_weight'],
+                  action_limit=params['action_limit'],
+                  target_speed_range=params['target_speed_range'],
+                  own_policy=own_policy,
+                  visualize=False, 
+                  traj_path=traj_path)
 obs = env.reset()
 
 
-'''
+# '''
 policy_kwargs = dict(activation_fn=th.nn.Tanh,
-                     net_arch=[dict(pi=[256,256,192,128], vf=[256,256,192,128])])     # v=6
-model = PPO('MlpPolicy', env, verbose=0, policy_kwargs=policy_kwargs, learning_rate=learning_rate)
-model.learn(total_timesteps=80)
+                     net_arch=[dict(vf=[256,256,256], pi=[256,256,256,12])])     # v=6
+model = PPO('MlpPolicy', env, verbose=0, policy_kwargs=policy_kwargs, learning_rate=learning_rate, n_steps=128)
+model.learn(total_timesteps=params['total_timesteps'])
 
 # Test saving and loading
 model.save(f"{d}/muscle_l{v}")
 del model
-'''
 # '''
+'''
 model = PPO.load(f"{d}/muscle_l{v}", env = env)
 obs = env.reset()
-for i in range(1000):
+for i in range(10):
     action, _state = model.predict(obs, deterministic=True)
+    print(action)
     obs, reward, done, info = env.step(action)
     if done:
         obs = env.reset()
-# '''
+'''
 
 # for i in range(100):
 #     o, r, d, i = env.step(np.zeros(18))
