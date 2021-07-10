e0ddc24d7bf9f3897942be25bec3f0efe4ddc172
f73chen <grace.fenglin@gmail.com>
2021-07-10 12:29:24 -0400

    conda osim env able to train joint-humanoid

--- humanoid_single.py
+++ humanoid_single.py
@@ -13,14 +13,14 @@ from stable_baselines3.common.policies import ActorCriticPolicy
 from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 
-v = "10"
+v = "v1"
 d = "humanoid"
 log_path = f"{d}/humanoid_log_{v}/"
 
 env = gym.make('HumanoidBulletEnv-v0')
 env.render()
 
-'''
+
 def learning_rate(frac):
     return 1.0e-4*(np.exp(6*(frac-1)))
 
@@ -39,15 +39,15 @@ def learning_rate(frac):
 
 policy_kwargs = dict(activation_fn=th.nn.ReLU,                                          # ReLU instead of Tanh
                      net_arch=[dict(pi=[256,256,192,128], vf=[256,256,192,128])])       # v=10
-model = PPO('MlpPolicy', env, verbose=0, policy_kwargs=policy_kwargs, learning_rate=learning_rate, tensorboard_log=log_path)
+model = PPO('MlpPolicy', env, verbose=0, policy_kwargs=policy_kwargs, learning_rate=learning_rate)
 model.learn(total_timesteps=8000000)
 
 # Test saving and loading
 model.save(f"{d}/humanoid_l{v}")
 del model
-'''
 
-# '''
+
+'''
 model = PPO.load(f"{d}/humanoid_l{v}", env = env)
 obs = env.reset()
 for i in range(1000):
@@ -57,4 +57,4 @@ for i in range(1000):
     time.sleep(1/200)
     if done:
         obs = env.reset()
-# '''
+'''
