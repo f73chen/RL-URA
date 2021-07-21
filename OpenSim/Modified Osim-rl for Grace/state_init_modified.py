ddfcdbaf78635d6b18341615151b9dda7d7a6013
f73chen <grace.fenglin@gmail.com>
2021-07-21 19:49:17 -0400

    trained in 3 conditions

--- OpenSim/Modified Osim-rl for Grace/state_init_modified.py
+++ OpenSim/Modified Osim-rl for Grace/state_init_modified.py
@@ -17,10 +17,10 @@ from stable_baselines3.common.utils import set_random_seed
 
 from osim.env.osimMod36d import L2RunEnvMod
 
-params = {'reward_weight': [6.0, 1.0, 1.0, 0.4, 0.0, 1.0, 1.0, 0.0, 0.5, 1.0],
+params = {'reward_weight': [6.0, 1.0, 1.0, 0.4, 0.0, 1.0, 1.0, 0.0, 0.5, 3.0],
           #['forward', 'survival', 'torso', 'joint', 'stability', 'act', 'footstep', 'jerk', 'slide']
           'action_limit': [1]*18,
-          'time_limit': 100,
+          'time_limit': 1000,
           'stepsize': 0.01,
           'integrator_accuracy': 5e-5,
           'seed': 0,
@@ -28,9 +28,9 @@ params = {'reward_weight': [6.0, 1.0, 1.0, 0.4, 0.0, 1.0, 1.0, 0.0, 0.5, 1.0],
           'lr_a1': 1.0e-4,
           'lr_a2': 2, 
           'target_speed_range': [0.8,1.2],
-          'total_timesteps': 40000}
+          'total_timesteps': 1000000}
 
-v = "v9"
+v = "v4"
 d = "muscle"
 log_path = f"{d}/muscle_log_{v}/"
 
@@ -77,14 +77,14 @@ if __name__ ==  '__main__':
                                 stepsize=params['stepsize'], 
                                 reward_weight = params['reward_weight'], 
                                 action_limit = params['action_limit'], 
-                                visualize=False,
+                                visualize=True,
                                 traj_path=traj_path,
                                 integrator_accuracy=params['integrator_accuracy'], 
                                 target_speed_range = params['target_speed_range'], 
                                 own_policy=own_policy) 
                         for i in range(params['num_cpu'])])
 
-    print(env.observation_space)    # Box(0.0, 0.0, (36,), float32)
+    # print(env.observation_space)    # Box(0.0, 0.0, (36,), float32)
     # print(env.init_space)
 
     # env = L2RunEnvMod(reward_weight=params['reward_weight'],
@@ -96,17 +96,18 @@ if __name__ ==  '__main__':
     obs = env.reset()
 
 
-    # '''
+    '''
     policy_kwargs = dict(activation_fn=th.nn.Tanh,
                         net_arch=[dict(vf=[256,256,256], pi=[256,256,256,12])])     # v=6
-    model = PPO('MlpPolicy', env, verbose=0, policy_kwargs=policy_kwargs, learning_rate=learning_rate, n_steps=128)
+    # model = PPO('MlpPolicy', env, verbose=0, policy_kwargs=policy_kwargs, learning_rate=learning_rate, n_steps=128)
+    model = PPO.load(f"{d}/muscle_lv3", env = env)
     model.learn(total_timesteps=params['total_timesteps'])
 
     # Test saving and loading
     model.save(f"{d}/muscle_l{v}")
     del model
-    # '''
     '''
+    # '''
     model = PPO.load(f"{d}/muscle_l{v}", env = env)
     obs = env.reset()
     for i in range(1000):
@@ -115,7 +116,7 @@ if __name__ ==  '__main__':
         obs, reward, done, info = env.step(action)
         if done:
             obs = env.reset()
-    '''
+    # '''
 
     # for i in range(100):
     #     o, r, d, i = env.step(np.zeros(18))
