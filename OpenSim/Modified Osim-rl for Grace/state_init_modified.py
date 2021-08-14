6231af0ab1798dc3f70a66ac178a60d79480a1fe
Fenglin Chen <55363139+f73chen@users.noreply.github.com>
2021-08-14 13:05:24 -0400

    v21 params

--- OpenSim/Modified Osim-rl for Grace/state_init_modified.py
+++ OpenSim/Modified Osim-rl for Grace/state_init_modified.py
@@ -23,17 +23,17 @@ from osim.env.osimMod36d import L2RunEnvMod
 params = {'reward_weight': [6.0, 1.0, 1.0, 0.4, 0.0, 1.0, 1.0, 0.0, 0.5, 1.0],
           #['forward', 'survival', 'torso', 'joint', 'stability', 'act', 'footstep', 'jerk', 'slide', 'mimic']
           'action_limit': [1]*18,
-          'time_limit': 61,
+          'time_limit': 120,
           'stepsize': 0.01,
           'integrator_accuracy': 5e-5,
           'seed': 0,
           'num_cpu': 12,
           'lr_a1': 1.0e-4,
           'lr_a2': 2,
-          'target_speed_range': [1.0, 1.0],
-          'total_timesteps': 1000000}
+          'target_speed_range': [1.2, 1.2],
+          'total_timesteps': 4000000}
 
-v = "v20"
+v = "v21"
 d = "muscle"
 log_dir = f"{d}/muscle_log_{v}/"
 tb_dir = log_dir + "tb/"
@@ -71,7 +71,7 @@ def make_env(env_in, rank, time_limit, seed=0, stepsize=0.01, **kwargs):
     return _init
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-traj_path = dir_path + "\\traj\\" + "1.0_gaitPrediction_solution_fullStride.sto"
+traj_path = dir_path + "\\traj\\" + "1.2_gaitPrediction_solution_fullStride.sto"
 
 
 
@@ -153,10 +153,13 @@ if __name__ ==  '__main__':
 
     # '''
     # Decrease mimic reward over time
-    iter_params = [{'time_limit': 30, 'reward_weight': [3.0, 1.0, 1.0, 0.4, 0.0, 1.0, 1.0, 0.0, 0.5, 3.0]},
-                   {'time_limit': 45, 'reward_weight': [2.5, 1.0, 1.0, 0.4, 0.0, 1.0, 1.0, 0.0, 0.5, 2.5]},
-                   {'time_limit': 60, 'reward_weight': [2.0, 1.0, 1.0, 0.4, 0.0, 1.0, 1.0, 0.0, 0.5, 2.0]},
-                   {'time_limit': 70, 'reward_weight': [1.5, 1.0, 1.0, 0.4, 0.0, 1.0, 1.0, 0.0, 0.5, 1.5]}]
+    iter_params = [{'time_limit': 30,   'reward_weight': [6.0, 1.0, 1.0, 0.4, 0.0, 1.0, 1.0, 0.0, 0.5, 3.5]},
+                   {'time_limit': 60,   'reward_weight': [6.0, 1.0, 1.0, 0.4, 0.0, 1.0, 1.0, 0.0, 0.5, 3.0]},
+                   {'time_limit': 90,   'reward_weight': [6.0, 1.0, 1.0, 0.4, 0.0, 1.0, 1.0, 0.0, 0.5, 2.5]},
+                   {'time_limit': 120,  'reward_weight': [6.0, 1.0, 1.0, 0.4, 0.0, 1.0, 1.0, 0.0, 0.5, 2.0]},
+                   {'time_limit': 150,  'reward_weight': [6.0, 1.0, 1.0, 0.4, 0.0, 1.0, 1.0, 0.0, 0.5, 1.5]},
+                   {'time_limit': 250,  'reward_weight': [6.0, 1.0, 1.0, 0.4, 0.0, 1.0, 1.0, 0.0, 0.5, 1.0]},
+                   {'time_limit': 1000, 'reward_weight': [6.0, 1.0, 1.0, 0.4, 0.0, 1.0, 1.0, 0.0, 0.5, 0.5]}]
 
     envs = [iter_env(**ip) for ip in iter_params]
 
