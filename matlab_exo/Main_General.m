5f7e5a5d8a4f8343c6fdf6e1dacd98bc339ecebf
f73chen <grace.fenglin@gmail.com>
2022-03-07 17:48:04 -0500

    code formatting

--- matlab_exo/Main_General.m
+++ matlab_exo/Main_General.m
@@ -2,12 +2,12 @@ clear all;clc;close all;
 load href_new;
 
 %% Constants and Params:
-motorSet={'hip_r','knee_r','ankle_r','hip_l','knee_l','ankle_l','back'};
-muscleSet={'hamstrings_r','bifemsh_r','glut_max_r','iliopsoas_r','rect_fem_r','vasti_r','gastroc_r','soleus_r','tib_ant_r','hamstrings_l','bifemsh_l','glut_max_l','iliopsoas_l','rect_fem_l','vasti_l','gastroc_l','soleus_l','tib_ant_l'};
+motorSet = {'hip_r','knee_r','ankle_r','hip_l','knee_l','ankle_l','back'};
+muscleSet = {'hamstrings_r','bifemsh_r','glut_max_r','iliopsoas_r','rect_fem_r','vasti_r','gastroc_r','soleus_r','tib_ant_r','hamstrings_l','bifemsh_l','glut_max_l','iliopsoas_l','rect_fem_l','vasti_l','gastroc_l','soleus_l','tib_ant_l'};
 d2r = pi/180;
 %% simulation parameters
-for i=1:1 
-    dt_0=0.002;     % controller update referesh rate
+for i = 1 
+    dt_0 = 0.002;     % controller update referesh rate
     updT = 0.01;    % save and GUI referesh rate
     phase = 0;      % initial phase of the motion. phase = 0 starts from the heel strike of the right leg
     phi = 0.61595;  % right-left phase difference (it has to be always equal to 1.2319/2)
@@ -17,8 +17,7 @@ for i=1:1
     T=60;           % simulation period
 end
 %% Controller and Force parameters
-
-for i=1:1 %human parameters
+for i = 1 %human parameters
     pelvis_stiffness = .0*[500 0 0]; pelvis_damping = .0*[50 0 0];  % tilt,x,y
     init_speed_gain =1;                                             % healthy human only
     p_gain = 3*[[10 2 5] 1*[10 2 5] 15]*60;                         % internal controller P gain
@@ -36,28 +35,28 @@ for m=1:6
         x0.(motorSet{m}) = d2r*ref.(motorSet{m-3})(0+phase+phi);                    
         v0.(motorSet{m}) = d2r*differentiate(ref.(motorSet{m-3}),0+phase+phi);
         v0.(motorSet{m}) = init_speed_gain*v0.(motorSet{m});   % initial speed gain is for having a stable transient form standing to walking
-        
     else    % for the right leg
         x0.(motorSet{m}) = d2r*ref.(motorSet{m})(phase);
         v0.(motorSet{m}) = d2r*differentiate(ref.(motorSet{m}),phase);
         v0.(motorSet{m}) = init_speed_gain*v0.(motorSet{m});
     end
 end
-x0.back=-0.42; % initial torso joint angle
+
+x0.back = -0.42; % initial torso joint angle
 v0.back = 0;   % initial torso joint velocity
 %% initiating the simulation
 % get instance form Human_Exo class and call it sub
 sub = Human_Exo_v02(x0,v0,updT,p_gain,d_gain,sat,pelvis_stiffness,pelvis_damping,exo_enable, mus_enable);
 % set the muscle activation and the external forces equal to zero
-for m=1:7
+for m = 1:7
         f_exo.(motorSet{m}) = 0;
 end
-for m=1:18
+for m = 1:18
         activation.(muscleSet{m}) = 0;
 end
 %% Simulation loop 
    
-for i=1:(T/dt_0)+1
+for i = 1:(T/dt_0)+1
     % comute simulation time
     t = (i-1)*dt_0; 
     % compute each leg's gait cycle and 
@@ -67,18 +66,17 @@ for i=1:(T/dt_0)+1
     % the iternal controllers of the system needs that. It is actually sets the
     % rest length for each joints spring. 
     sub.step(t+dt_0,f_exo,activation,r_human);
-    
 end
 
 %% Functions
 function [s_r,s_l,r] = GaitGen(t,href,motorSet,phase,phi)
-    T=1.2319;
+    T = 1.2319;
     t_l = t+phase+phi;
     t_r = t+phase;
     s_r = mod(t_r,T)/T;
     s_l = mod(t_l,T)/T;
     r=zeros(7,1);
-    for m=1:7
+    for m = 1:7
         if ismember(m,[4,5,6])
             r(m,1)=deg2rad(href.(motorSet{m-3})(s_l*T));
         else
