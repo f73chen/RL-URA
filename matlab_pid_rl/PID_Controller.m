972897305773e6b85a34538e1fb50b7c715ba0bc
f73chen <grace.fenglin@gmail.com>
2022-03-27 13:41:48 -0400

    Create PID_Controller.m

---
+++ matlab_pid_rl/PID_Controller.m
@@ -0,0 +1,8 @@
+function [gain] = PID_Controller(state, reward)
+    % Actions: increase or decrease gain by 5%
+    % Parameters:
+    %   state: max absolute error for each phase during the previous cycle
+    %   reward: deviation from reference trajectory in the previous cycle
+
+end
+
